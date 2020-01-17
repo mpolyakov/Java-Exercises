cb25a69b979f1c3db2a23d1eabc8551396667cc4
mpolyakov <polyakov4@yandex.ru>
2020-01-17 23:47:44 +0300

    добавил возможность соединения по HTTPS без проверки SSL сертификата

--- HttpConnToCodec/src/HttpConnection.java
+++ HttpConnToCodec/src/HttpConnection.java
@@ -1,7 +1,11 @@
-import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.*;
 import java.io.*;
 import java.net.HttpURLConnection;
+import java.net.InetAddress;
 import java.net.URL;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
 import java.util.Base64;
 
 
@@ -9,14 +13,71 @@ import java.util.Base64;
 public class HttpConnection {
     public static URL url = null;
     public static String body = null;
+    public String ipAdress = "10.65.26.10";
+
+    //Отключение проверки сертификата ->
+    static {
+        disableSslVerification();
+    }
+
+    private static void disableSslVerification() {
+        try
+        {
+            // Create a trust manager that does not validate certificate chains
+            TrustManager[] trustAllCerts = new TrustManager[] {new X509TrustManager() {
+                public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                    return null;
+                }
+                public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                }
+                public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                }
+            }
+            };
+
+            // Install the all-trusting trust manager
+            SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+
+            // Create all-trusting host name verifier
+            HostnameVerifier allHostsValid = new HostnameVerifier() {
+                public boolean verify(String hostname, SSLSession session) {
+                    return true;
+                }
+            };
+
+            // Install the all-trusting host verifier
+            HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (KeyManagementException e) {
+            e.printStackTrace();
+        }
+    }
+    // <- Отключение проверки сертификата
 
     public static void main(String[] args) {
-        body = "<Configuration> <SystemUnit> <Name>Переговорная Основа Лаб</Name> </SystemUnit> </Configuration>";
+        //Reachability
+        try{
+            InetAddress address = InetAddress.getByName("10.65.26.10");
+            boolean reachable = address.isReachable(10000);
+
+            System.out.println("Проверка доступности видеотерминала: " + reachable);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        //Reachability
+
+
+
+//        body = "<Configuration> <SystemUnit> <Name>Переговорная Основа Laab</Name> </SystemUnit> </Configuration>";
+        body = "<Configuration> <NetworkServices> <HTTP> <Mode>HTTP+HTTPS</Mode> </HTTP> </NetworkServices> </Configuration>";
 //        body = "<Configuration> <NetworkServices> <Websocket>FollowHTTPService</Websocket> </NetworkServices> </Configuration>";
 
         try {
 //            url = new URL ("http://10.65.26.10/status.xml");                                         //1-GET
-            url = new URL ("http://10.65.26.10/putxml");                                                    //2-POST
+            url = new URL ("https://10.65.26.10/putxml");                                                    //2-POST
 
             String userCredentials = "operator:password";
             String basicAuth = "Basic " + new String(Base64.getEncoder().encode(userCredentials.getBytes()));
