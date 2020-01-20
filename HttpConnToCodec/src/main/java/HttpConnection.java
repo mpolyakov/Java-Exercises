c887e0d170c1fda9ffe415f76173c2491ad5c7ae
mpolyakov <polyakov4@yandex.ru>
2020-01-20 21:01:57 +0300

    некоторые изменения в коде

--- HttpConnToCodec/src/main/java/HttpConnection.java
+++ HttpConnToCodec/src/main/java/HttpConnection.java
@@ -9,7 +9,6 @@ import java.security.cert.X509Certificate;
 import java.util.Base64;
 
 
-
 public class HttpConnection {
     public static URL url = null;
     public static String body = null;
@@ -58,7 +57,7 @@ public class HttpConnection {
     // <- Отключение проверки сертификата
 
     public static void main(String[] args) {
-        //Reachability
+        //Проверка доступности узла
         try{
             InetAddress address = InetAddress.getByName("10.65.26.10");
             boolean reachable = address.isReachable(10000);
@@ -67,25 +66,25 @@ public class HttpConnection {
         } catch (Exception e){
             e.printStackTrace();
         }
-        //Reachability
+        //Проверка доступности узла
 
 
 
-//        body = "<Configuration> <SystemUnit> <Name>Переговорная Основа Laab</Name> </SystemUnit> </Configuration>";
-        body = "<Configuration> <NetworkServices> <HTTP> <Mode>HTTP+HTTPS</Mode> </HTTP> </NetworkServices> </Configuration>";
+        body = "<Configuration> <SystemUnit> <Name>Переговорная Основа Лаб</Name> </SystemUnit> </Configuration>";
+//        body = "<Configuration> <NetworkServices> <HTTP> <Mode>HTTP+HTTPS</Mode> </HTTP> </NetworkServices> </Configuration>";
 //        body = "<Configuration> <NetworkServices> <Websocket>FollowHTTPService</Websocket> </NetworkServices> </Configuration>";
 
         try {
-//            url = new URL ("http://10.65.26.10/status.xml");                                         //1-GET
-            url = new URL ("https://10.65.26.10/putxml");                                                    //2-POST
+            url = new URL ("http://10.65.26.10/status.xml");                                         //1-GET
+//            url = new URL ("https://10.65.26.10/putxml");                                                    //2-POST
 
             String userCredentials = "operator:password";
             String basicAuth = "Basic " + new String(Base64.getEncoder().encode(userCredentials.getBytes()));
 
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-//            connection.setRequestMethod("GET");                                                                  //1-GET
-            connection.setRequestMethod("POST");                                                                 //2-POST
-            connection.setRequestProperty("Content-Type", "text/xml");                                           //2-POST
+            connection.setRequestMethod("GET");                                                                  //1-GET
+//            connection.setRequestMethod("POST");                                                                 //2-POST
+//            connection.setRequestProperty("Content-Type", "text/xml");                                           //2-POST
 
             connection.setReadTimeout(10000);
             connection.setConnectTimeout(15000);
@@ -94,9 +93,9 @@ public class HttpConnection {
             connection.setRequestProperty("Authorization", basicAuth);
 
 
-            OutputStream output = new BufferedOutputStream(connection.getOutputStream());               //2-запись в поток и отправка
-            output.write(body.getBytes());                                                              //2-запись в поток и отправка
-            output.flush();                                                                             //2-запись в поток и отправка
+//            OutputStream output = new BufferedOutputStream(connection.getOutputStream());               //2-запись в поток и отправка
+//            output.write(body.getBytes());                                                              //2-запись в поток и отправка
+//            output.flush();                                                                             //2-запись в поток и отправка
 
             InputStream content = (InputStream)connection.getInputStream();                             //чтение из потока
             BufferedReader in = new BufferedReader (new InputStreamReader (content));
@@ -108,6 +107,7 @@ public class HttpConnection {
         } catch(Exception e) {
             e.printStackTrace();
         }
+
 
     }
 
